4cacc849f2264dae8c70781bcdaaf21f31afe118
jordan <jordan@jordans-MacBook-Air.local>
2020-01-27 22:28:15 +0700

    login works with greeting for username

--- api/server.js
+++ api/server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const server = express();
 const registerRoute= require("../register/registerRoute");
-//const bcrypt = require('bcrypt');
+const loginRoute= require("../login/loginRoutes");
 
 server.use(express.json());
 server.use("/api/register", registerRoute);
+server.use("/api/login", loginRoute);
 
 server.get("/", (req, res) => {
   res.send("<h1>test </h1>");
